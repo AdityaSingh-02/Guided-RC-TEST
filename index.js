a9dcffab6717e7e9b7fd76f6f2591b964f5c8205
Aditya Singh <asaditya2002@gmail.com>
2023-05-21 10:23:23 +0530

    Added Yaml Config for code tour

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var app = express();
+var port = 3002;
+app.listen(port, function () { return console.log("Running on port 3002"); });
---
+++ index.ts
@@ -0,0 +1,8 @@
+import * as express from 'express';
+const app = express()
+const port = 3002
+
+
+
+
+app.listen(port, () => console.log("Running on port 3002"))
